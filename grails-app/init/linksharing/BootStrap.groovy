67c9ba7efba9db17fd592546ba2ac84bd74496f9
himanshiiiii <himanshi.gupta@tothenew.com>
2018-03-30 17:24:16 +0530

    Himanshi-commit

--- grails-app/init/linksharing/BootStrap.groovy
+++ grails-app/init/linksharing/BootStrap.groovy
@@ -11,11 +11,9 @@ class BootStrap {
         createResource()
         subscribeTopicsNotCreatedByUser()
         createReadingItems()
-        question27()
-        createResourceRating()
-//        createReadingItemIfItDoesNotExistsInUsersReadingItem()
-        //   question24()
 
+//        FIX method arguments here
+//        createReadingItemIfItDoesNotExistsInUsersReadingItem()
 
     }
 
@@ -187,8 +185,7 @@ class BootStrap {
         }
 
 
-
-    void question24(User user,Topic topic)
+    void createReadingItemIfItDoesNotExistsInUsersReadingItem(User user,Topic topic)
     {
         topic.resources.each {
             ReadingItem readingItem=new ReadingItem(user: user,resource:it,isRead: false)
@@ -204,57 +201,9 @@ class BootStrap {
         }
     }
 
-    //question 26
-    void createResourceRating(){
-        List<User> userList=User.getAll()
-        userList.each {
-            User user->
-                user.readingItems.each {
-                    if(!it.isRead && ResourceRating.findAllByUser(user).size()==0){
-                        ResourceRating resourceRating=new ResourceRating(user: user, resource: it.resource,score:4)
-                        resourceRating.validate()
-                        if(resourceRating.save()){
-                            log.info("Saved Successfully")
-                            user.addToResourceRating(resourceRating)
-                            user.save()
-                        }
-                        else {
-                            log.error("${resourceRating.errors.getAllErrors()}")
-                        }
-                    }
-                }
-
-        }
-
-    }
 
 
 
-    void question27(){
-        List<ResourceRating> resourceRatingList=ResourceRating.getAll()
-        resourceRatingList.each {
-            ResourceRating resourceRating->
-                if(ReadingItem.findAllByUserAndResource(resourceRating.user,resourceRating.resource).size()==0) {
-                    ReadingItem readingItem = new ReadingItem(user: resourceRating.user, resource: resourceRating.resource, isRead: false)
-                    if (readingItem.save()){
-                        log.info("Saved Successfully")
-                        resourceRating.resource.addToReadingItems(readingItem)
-                        resourceRating.user.addToReadingItems(readingItem)
-                        resourceRating.resource.save()
-                        resourceRating.user.save()
-                    }
-                    else {
-                        log.error("Error:- ${readingItem.errors.getAllErrors()}")
-                    }
-                }
-        }
-    }
-
-
-
-
-
-
-    def destroy = {
+        def destroy = {
         }
     }
